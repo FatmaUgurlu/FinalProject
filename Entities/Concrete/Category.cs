5c138dc7ec35d67fe47bcb21d63d744addee6c0e
fatmaugurlu <fatmaugurlu89@hotmail.com>
2021-03-14 16:36:37 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Çıplak Class Kalmasın: concrete klasöründeki sınıflar bir veri tabanındaki veriye denk gelir. ınterface oluşturulur.
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
